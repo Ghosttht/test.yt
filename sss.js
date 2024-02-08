c886ecf89b9db48a2e11a79f0cb8e1bd6153ff64
Ghosttht <153176593+Ghosttht@users.noreply.github.com>
2024-02-08 12:40:02 +0400

    Add files via upload

---
+++ sss.js
@@ -0,0 +1,19 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+     var n = [0 , 1]
+     var y = n(0 + 1)
+    
+    //Write your code here:
+    
+    
+    
+fibonacciGenerator(5)
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
